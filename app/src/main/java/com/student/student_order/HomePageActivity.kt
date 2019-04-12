cf111c572ec25870131caaee45a670e1e96ebb1f
wei0820 <jheng.wei0919@gmail.com>
2019-04-12 19:04:47 +0800

    add homepageActivity

---
+++ app/src/main/java/com/student/student_order/HomePageActivity.kt
@@ -0,0 +1,17 @@
+package com.student.student_order
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_home_page.*
+
+class HomePageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+
+    }
+
+}
