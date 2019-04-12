2811cea4bb6d9b54fd7df86922d6b6fe135289f9
wei0820 <jheng.wei0919@gmail.com>
2019-04-12 19:07:01 +0800

    add orderActivity

---
+++ app/src/main/java/com/student/student_order/OrderActivity.kt
@@ -0,0 +1,18 @@
+package com.student.student_order
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import com.student.student_order_2.R
+
+import kotlinx.android.synthetic.main.activity_order.*
+
+class OrderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+
+    }
+
+}
