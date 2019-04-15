d3da402db51c87413aec9acf81d69088dc98edb9
wei0820 <jheng.wei0919@gmail.com>
2019-04-15 17:22:28 +0800

    add about me  and alertdilog

--- app/src/main/java/com/student/student_order_2/MyApi.java
+++ app/src/main/java/com/student/student_order_2/MyApi.java
@@ -1,5 +1,8 @@
 package com.student.student_order_2;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.text.format.DateFormat;
 import android.util.Log;
 
@@ -14,5 +17,17 @@ public class MyApi {
         cal.setTimeInMillis(time);
         String date = DateFormat.format("yyyy-MM-dd HH:mm:ss", cal).toString();
         return date;
+    }
+
+    public static void setAlertDilog(Context context,String title,String message){
+        new AlertDialog.Builder(context)
+                .setTitle(title)
+                .setMessage(message)
+                .setNegativeButton("確定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).show();
     }
 }
--- app/src/main/java/com/student/student_order_2/SettingActivity.kt
+++ app/src/main/java/com/student/student_order_2/SettingActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button
 import com.facebook.CallbackManager
 import com.facebook.FacebookSdk
 import com.facebook.appevents.AppEventsLogger
@@ -16,6 +17,8 @@ import kotlinx.android.synthetic.main.activity_setting.*
 class SettingActivity : AppCompatActivity() {
     var callbackManager: CallbackManager? = null
     lateinit var mFbLoginBtn : LoginButton
+    lateinit var mButton:Button
+    lateinit var mButton2: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         callbackManager = CallbackManager.Factory.create()
@@ -27,6 +30,17 @@ class SettingActivity : AppCompatActivity() {
         mFbLoginBtn = findViewById(R.id.login_button)
         FacebookManager.printHashKey(this)
         FacebookManager.fbLogin(this,mFbLoginBtn,callbackManager,SettingActivity::class.java)
+        mButton = findViewById(R.id.button)
+        mButton2 = findViewById(R.id.button2)
+        mButton.setOnClickListener {
+            MyApi.setAlertDilog(this,"關於我們","嶺東科技大學資訊網路系\n\n粘竣凱\n陳盈臻 李俊逸 張祐源 林信局")
+
+        }
+        mButton2.setOnClickListener {
+            MyApi.setAlertDilog(this,"本店資訊","30年代的傳統口味的精緻料理，且是一種深具魅力的料理藝術，我們古法調配的湯頭加上純手工製作的手工麵線以及我們老闆自己親手滷的大腸和自己炒的辣蘿蔔，經過精心調配再現傳統美味")
+
+        }
+
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
