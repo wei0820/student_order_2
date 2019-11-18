da4bb259d071214b654468e8ea3b6517025ca543
Wei Wei <jheng.wei0919@gmail.com>
2019-11-18 13:57:43 +0800

    fix view

---
+++ app/src/main/java/com/student/student_order_2/ShopNewsActivity.kt
@@ -0,0 +1,13 @@
+package com.student.student_order_2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ShopNewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_news)
+        title = "本店資訊"
+    }
+}
